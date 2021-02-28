81358a1f568d9d234a4229d9106bcb8a17634094
Lmontoya10 <luisamontoya250516@correo.itm.edu.co>
2021-02-28 14:12:11 -0500

    add navigation

---
+++ navigatios/TopRestaurantsStack.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import Search from '../screens/Search'
+import TopRestaurants from '../screens/TopRestaurants'
+
+
+const Stack = createStackNavigator()
+
+
+export default function TopRestaurantsStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name= "top-restaurants"
+                component= {TopRestaurants}
+                options = {{ title: "Los Mejores Restaurantes"}
+            }
+            />
+        </Stack.Navigator>
+        
+    )
+}
