5784c2b303254da8fecb47f8cb02b5e9ef9906bf
Lmontoya10 <luisamontoya250516@correo.itm.edu.co>
2021-03-12 11:57:04 -0500

    user register and change photo

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 
 import React from 'react';
 import Navigation from './navigatios/Navigation';
+import {LogBox} from 'react-native'
 
+LogBox.ignoreAllLogs()
 export default function App() {
   return (
    <Navigation/>
---
+++ components/account/RegisterForm.js
@@ -0,0 +1,154 @@
+import { size } from 'lodash'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Button, Icon, Input } from 'react-native-elements'
+import {useNavigation} from '@react-navigation/native'
+
+import { validateEmail } from '../../utils/helpers'
+import {registerUser} from '../../utils/actions'
+import Loading from '../Loading'
+
+
+
+export default function RegisterForm() {
+    const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFromValues())
+    const [errorEmail, setErrorEmail] = useState("")
+    const [errorPasword, setErrorPasword] = useState("")
+    const [errorConfirm, setErrorConfirm] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const navigation = useNavigation()
+
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text })
+
+    }
+
+    const doRegisterUser =async () => {
+        if (!validateData()) {
+            return
+        }
+        setLoading(true)
+        const result = await registerUser(formData.email, formData.password)
+        setLoading(false)
+        if (!result.statusResponse) {
+            setErrorEmail(result.error)
+            return
+        }
+
+        navigation.navigate("account")
+    }
+
+    const validateData = () => {
+        setErrorConfirm("")
+        setErrorEmail("")
+        setErrorPasword("")
+        let isValid = true
+
+
+        if (!validateEmail(formData.email)) {
+            setErrorEmail("Debes ingresar un email valido")
+            isValid = false
+        }
+        if (size(formData.password) < 6) {
+            setErrorPasword("Debes ingresar una contraseña de 6 o mas caracteres")
+            isValid = false
+        }
+        if (size(formData.confirm) < 6) {
+            setErrorConfirm("Debes ingresar una confirmacion de contraseña de 6 o mas caracteres")
+            isValid = false
+        }
+        if (formData.password !== formData.confirm) {
+            setErrorPasword("La contraseña y la confirmacion no son iguales")
+            setErrorConfirm("La contraseña y la confirmacion no son iguales")
+            isValid = false
+        }
+        return isValid
+    }
+
+    return (
+        <View style={styles.form}>
+            <Input
+                containerStyle={styles.input}
+                placeholder="Ingresa tu email..."
+                onChange={(e) => onChange(e, "email")}
+                keyboardType="email-address"
+                errorMessage={errorEmail}
+                defaultValue={formData.email}
+            />
+            <Input
+                containerStyle={styles.input}
+                placeholder="Ingresa tu contraseña..."
+                password={true}
+                secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "password")}
+                errorMessage={errorPasword}
+                defaultValue={formData.password}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
+            />
+            <Input
+                containerStyle={styles.input}
+                placeholder="Confirma tu contraseña..."
+                password={true}
+                secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "confirm")}
+                errorMessage={errorConfirm}
+                defaultValue={formData.confirm}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
+            />
+            <Button
+                title="Registrar nuevo usuario"
+                containerStyle={styles.btnContainer}
+                buttonStyle={styles.btn}
+                onPress={() => doRegisterUser()}
+
+            />
+            <Loading
+            isVisible={loading}
+            text="Creando cuenta...."
+            />
+        </View>
+    )
+}
+
+const defaultFromValues = () => {
+    return { email: "", password: "", confirm: "" }
+}
+
+const styles = StyleSheet.create({
+    form: {
+        marginTop: 30,
+
+    },
+    input: {
+        width: "100%"
+    },
+    btnContainer: {
+        marginTop: 20,
+        width: "95%",
+        alignSelf: "center"
+    },
+    btn: {
+        backgroundColor: "#442484"
+    },
+    icon: {
+        color: "#c1c1c1"
+    }
+
+})
